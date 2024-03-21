5e6b238adcd1c46fc42a7dcaffa8ca81f3040be3
Oliveira-Renato <renato.pritf@gmail.com>
2024-03-21 17:09:27 -0300

    second commit

---
+++ src/app/index/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>React Native na prática!</Text>
+    </View>
+  )
+}
---
+++ src/app/index/styles.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "red",
+    flex: 1
+  },
+})
