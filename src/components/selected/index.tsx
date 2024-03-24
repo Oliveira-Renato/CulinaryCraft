8b578d9bd8765e9ee0e8421a82041bb74f3e75a6
Oliveira-Renato <renato.pritf@gmail.com>
2024-03-23 23:35:09 -0300

    feature: Component Selected

--- src/components/selected/index.tsx
+++ src/components/selected/index.tsx
@@ -1,3 +1,22 @@
-export function Selected() {
-  return
+import Animated from "react-native-reanimated"
+import { Text, View } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
+
+import { styles } from "./styles"
+
+type SelectedProps = {
+  quantity: number
+  onClear: () => void
+  onSearch: () => void
+}
+
+export function Selected({ quantity, onClear, onSearch }: SelectedProps) {
+  return (
+    <Animated.View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.label}>{quantity} ingredientes selecionados.</Text>
+        <MaterialIcons name="close" size={24} onPress={onClear} />
+      </View>
+    </Animated.View>
+  )
 }
