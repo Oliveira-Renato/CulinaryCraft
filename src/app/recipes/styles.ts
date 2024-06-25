4b20171d0a409bcf2ed260575b0b8aa1d2743a7d
Oliveira-Renato <renato.pritf@gmail.com>
2024-06-25 13:29:41 -0300

    recipes styles and supabase

--- src/app/recipes/styles.ts
+++ src/app/recipes/styles.ts
@@ -1,3 +1,4 @@
+import { theme } from "@/theme";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
@@ -10,6 +11,8 @@ export const styles = StyleSheet.create({
     marginBottom: 12,
   },
   title: {
-
+    fontSize: theme.fonts.size.heading.md,
+    fontFamily: theme.fonts.family.bold,
+    marginTop: 22
   }
 })
