220ed1575251b870c534f66d2fba72d015c4a2d6
Oliveira-Renato <renato.pritf@gmail.com>
2024-06-25 14:15:25 -0300

    conection with sb done

--- src/components/ingredients/index.tsx
+++ src/components/ingredients/index.tsx
@@ -1,52 +1,60 @@
-import { Alert, ScrollView } from "react-native"
-import {router} from "expo-router"
-import { styles } from "./styles"
-import { Ingredient } from "../ingredient"
-import { useState } from "react"
-import { SequencedTransition } from "react-native-reanimated"
-import { Selected } from "../selected"
+import { Alert, ScrollView } from "react-native";
+import { router } from "expo-router";
+import { styles } from "./styles";
+import { Ingredient } from "../ingredient";
+import { useEffect, useState } from "react";
+import { SequencedTransition } from "react-native-reanimated";
+import { Selected } from "../selected";
+import { services } from "@/services";
 
 export function Ingredients() {
-  const [selected, setSelected] = useState<string[]>([])
+  const [selected, setSelected] = useState<string[]>([]);
 
   const handleToggleSelected = (value: string) => {
     if (selected.includes(value)) {
-      return setSelected((state) => state.filter((item) => item !== value))
+      return setSelected((state) => state.filter((item) => item !== value));
     }
 
-    setSelected((state) => [...state, value])
-  }
+    setSelected((state) => [...state, value]);
+  };
 
   const handleClearSelected = () => {
     Alert.alert("Limpar", "Deseja limpar tudo?", [
-      { text: "Não", style: "cancel"},
-      { text: "Sim", onPress: () => setSelected([])}
-    ])
-  }
+      { text: "Não", style: "cancel" },
+      { text: "Sim", onPress: () => setSelected([]) },
+    ]);
+  };
 
   const handleSearch = () => {
-    router.navigate("/recipes/")
-  }
+    router.navigate("/recipes/");
+  };
+
+  useEffect(() => {
+    services.ingredients.findsAll().then(console.log);
+  }, []);
 
   return (
-    <ScrollView contentContainerStyle={styles.container} showsVerticalScrollIndicator={false}>
+    <ScrollView
+      contentContainerStyle={styles.container}
+      showsVerticalScrollIndicator={false}
+    >
       {Array.from({ length: 100 }).map((item, index) => (
-        <Ingredient key={index}
+        <Ingredient
+          key={index}
           name="Maça"
           image=""
           selected={selected.includes(String(index))}
-          onPress={() => handleToggleSelected(String(index))} />
+          onPress={() => handleToggleSelected(String(index))}
+        />
       ))}
 
-      {
-        selected.length > 0 && (
-          <Selected
+      {selected.length > 0 && (
+        <Selected
           quantity={selected.length}
           onClear={handleClearSelected}
           onSearch={handleSearch}
         />
-        )
-      }
+      )}
     </ScrollView>
-  )
-} 
+  );
+}
---
+++ src/services/index.tsx
@@ -0,0 +1,5 @@
+import * as ingredients from "./ingredientsServices";
+
+export const services = {
+  ingredients,
+};
